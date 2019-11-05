9f1256a5cc4e28913298189218d4c8de2b109d43
tuananh_bk <tuananh.kirimaru@gmail.com>
2019-11-06 00:29:29 +0900

    ok

--- app/db/migration/default/V2_1__addon.scala
+++ app/db/migration/default/V2_1__addon.scala
@@ -1,5 +1,7 @@
 package db.migration.default
 
+package db.migration.default
+
 import slick.jdbc.MySQLProfile.api._
 import slick.migration.api._
 import slick.migration.api.flyway._
@@ -7,14 +9,16 @@ import slick.migration.api.flyway.UnmanagedDatabase
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.Await
 
-class V2__scala_migrate extends BaseJavaMigration {
+
+
+class V2_1__scala_addon extends BaseJavaMigration {
 
   lazy val db = Database.forConfig("db.default")
 
-  class TestTable(tag: Tag) extends Table[(Int, Int)](tag, "testtable") {
+  class TestTable(tag: Tag) extends Table[(Int, Int)](tag, "testtable2") {
     val col1 = column[Int]("col1")
     val col2 = column[Int]("col2")
     def * = (col1, col2)
--- app/db/migration/default/V3__scala_test.scala
+++ app/db/migration/default/V3__scala_test.scala
@@ -7,24 +7,27 @@ import slick.migration.api.flyway.UnmanagedDatabase
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.migration.BaseJavaMigration
 import org.flywaydb.core.api.migration.Context
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.Await
 
-class V2__scala_migrate extends BaseJavaMigration {
+
+
+class V3__scala_test extends BaseJavaMigration {
 
   lazy val db = Database.forConfig("db.default")
 
-  class TestTable(tag: Tag) extends Table[(Int, Int)](tag, "testtable") {
+  class TestTable(tag: Tag) extends Table[(Int, Int, Int)](tag, "testtable") {
     val col1 = column[Int]("col1")
     val col2 = column[Int]("col2")
-    def * = (col1, col2)
+    val col3 = column[Int]("col3")
+    def * = (col1, col2, col3)
   }
   val testTable = TableQuery[TestTable]
 
   implicit val dialect: MySQLDialect = new MySQLDialect
 
-  val m1 = TableMigration(testTable).create
-    .addColumns(_.col1, _.col2)
+  val m1 = TableMigration(testTable)
+    .addColumns(_.col3)
 
   def migrate(context: Context): Unit = {
     db.run(m1())
